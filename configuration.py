ef776bd3abcd656c767999a8ad43da429951fe95
Karamizova <laurakaramiz@yandex.ru>
2024-02-14 19:57:28 +0000

    Описание изменений

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://cbe092fe-47b8-4c60-a881-ae0e7c31d139.serverhub.praktikum-services.ru"
+CREATE_COURIER = "/api/v1/courier"
+CREATE_ORDER = "/api/v1/orders"
+ORDER_INFORMATION = "/api/v1/orders/track?t="
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Карамизова",
+    "lastName": "Лаура",
+    "address": "Neva, 137 apt.",
+    "metroStation": 204,
+    "phone": "+7 928 081 60 15",
+    "rentTime": 5,
+    "deliveryDate": "2024-02-12",
+    "comment": "Привет!",
+    "color": [
+        "BLACK"
+    ]
+}
